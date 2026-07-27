0603e54d7d79a20fea36ca7988358f3931068941
Ashish Pardeshi <ashu.pardeshi714@gmail.com>
2026-07-27 19:37:16 +0530

    Add files via upload

---
+++ employee_data.sql
@@ -0,0 +1,26 @@
+INSERT INTO department (department_name)
+VALUES
+('HR'),
+('IT'),
+('Finance'),
+('Sales'),
+('Marketing');
+
+INSERT INTO employee
+(name, age, gender, salary, department_id, hire_date, city)
+VALUES
+('Ashish',24,'Male',55000,2,'2024-01-15','Pune'),
+('Rahul',28,'Male',65000,2,'2023-05-20','Mumbai'),
+('Priya',26,'Female',50000,1,'2022-02-10','Pune'),
+('Sneha',29,'Female',72000,3,'2021-08-15','Nashik'),
+('Rohit',31,'Male',80000,4,'2020-09-12','Delhi'),
+('Amit',27,'Male',62000,2,'2023-11-01','Pune'),
+('Neha',30,'Female',68000,5,'2022-06-18','Nagpur'),
+('Karan',25,'Male',45000,1,'2024-03-11','Mumbai'),
+('Pooja',32,'Female',90000,3,'2019-07-22','Pune'),
+('Vikas',29,'Male',75000,4,'2021-12-05','Hyderabad'),
+('Anjali',26,'Female',56000,5,'2024-02-01','Pune'),
+('Suresh',35,'Male',95000,2,'2018-01-10','Bangalore'),
+('Riya',23,'Female',40000,1,'2025-01-12','Pune'),
+('Ajay',34,'Male',87000,3,'2019-09-30','Mumbai'),
+('Meena',28,'Female',60000,5,'2023-04-14','Nagpur');
